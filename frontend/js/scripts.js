92854cdf0991fe3c67b33a398e4189c14b24644f
Brun02K23 <bvirinni@gmail.com>
2024-05-11 10:38:41 -0300

    first commit

---
+++ frontend/js/scripts.js
@@ -0,0 +1,20 @@
+const apiUrl = 'http://localhost:3000/paquetes'; // Reemplaza con la URL de tu API
+
+// Función para cargar la grilla de paquetes
+function cargarPaquetes() {
+}
+
+// Función para buscar paquetes por descripción
+function buscarPaquetes() {
+}
+
+// Función para agregar un nuevo paquete
+function agregarPaquete() {
+}
+
+// Función para eliminar un paquete
+function eliminarPaquete(id) {
+}
+
+// Cargar la lista de paquetes al cargar la página
+cargarPaquetes();
